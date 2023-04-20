3c652677c62cbb697a042c3e7098fab52a762086
MalshaImashi <126412247+MalshaImashi@users.noreply.github.com>
2023-04-20 21:54:42 +0530

    Update add.py

--- add.py
+++ add.py
@@ -10,3 +10,24 @@ def computeMasterKey(mp,ds):
 	salt = ds.encode()
 	key = PBKDF2(password, salt, 32, count=1000000, hmac_hash_module=SHA512)
 	return key
+
+def addEntry(mp, ds, sitename, siteurl, email, username);
+       # get the password
+	password = getpass("password: ")
+	
+	
+	# compute master key
+	mk=computeMasterkey(mp, ds)
+	
+	# encrypt password with mk
+	encrypted = utils.aesutil.encrypt(key=mk, source=password, keyType="bytes")
+	
+	# Add to db
+	db = dbconfig()
+	cursor = db.cursor()
+	query = "INSERT INTO pm.entries (sitename, siteurl, email, username, password) values (%s, %s, %s, %s, %s)"
+	val = (sitename,siteurl,email,username,encrypted)
+	cursor.execute(query, val)
+	db.commit()
+
+	printc("[green][+][/green] Added entry ")
