c81ba9f4213c6c05770a9fdc258bde0a905597b2
MalshaImashi <126412247+MalshaImashi@users.noreply.github.com>
2023-04-21 17:50:19 +0530

    Update retrieve.py

--- retrieve.py
+++ retrieve.py
@@ -1,47 +1,47 @@
-from utils.dbconfig import dbconfig
+from utils.dbconfig import dbconfig   # import database configuration
+from rich import print as printc   # import the rich library for pretty printing
+from rich.console import Console   # import Console from rich library for more advanced console output
+from rich.table import Table   # import Table from rich library for creating tables
 
-from rich import print as printc
-from rich.console import Console
-from rich.table import Table
+import utils.aesutil   # import aesutil module from utils package
+import pyperclip   # import pyperclip for copying text to clipboard
 
-import utils.aesutil
-import pyperclip
+from getpass import getpass   # import getpass to get password from user input
+from Crypto.protocol.KDF import PBKDF2   # import PBKDF2 from Crypto library for password-based key derivation
+from Crypto.Hash import SHA512   # import SHA512 from Crypto library for hash function
+from Crypto.Random import get_random_bytes   # import get_random_bytes from Crypto library for generating random bytes
+python
+Copy code
+def computeMasterKey(mp, ds):
+	password = mp.encode()   # encode the master password
+	salt = ds.encode()   # encode the database salt
+	key = PBKDF2(password, salt, 32, count=1000000, hmac_hash_module=SHA512)   # derive a key using PBKDF2
+	return key   # return the derived key
+	
+#This function takes two parameters - mp (the master password) and ds (the database salt), encodes them as bytes, and uses PBKDF2 to derive a 32-byte key by using the SHA512 hash function with 1,000,000 iterations. The function returns the derived key.
 
-from getpass import getpass
-from Crypto.protocol.KDF import PBKDF2
-from Crypto.Hash import SHA512
-from Crypto.Random import get_random_bytes
+python
+Copy code
+def retrieveEntries(mp, ds, search, decryptPassword=False):
+	db = dbconfig()   # connect to the database using dbconfig
+	cursor = db.cursor()   # create a cursor object
 
-
-def computeMasterKey(mp,ds):
-	password = mp.encode()
-	salt = ds.encode()
-	key = PBKDF2(password, salt, 32, count=1000000, hmac_hash_module=SHA512)
-	return key
-
-
-def retrieveEntries(mp, ds, search, decryptPassword = False):
-	db = dbconfig()
-	cursor = db.cursor()
-  
-  query = ""
-	if len(search)==0:
+	if len(search) == 0:
 		query = "SELECT * FROM pm.entries"
 	else:
 		query = "SELECT * FROM pm.entries WHERE "
 		for i in search:
-			query+=f"{i} = '{search[i]}' AND "
+			query += f"{i} = '{search[i]}' AND "
 		query = query[:-5]
 
-	cursor.execute(query)
-	results = cursor.fetchall()
-  
-  if len(results) == 0:
+	cursor.execute(query)   # execute the SQL query
+	results = cursor.fetchall()   # fetch all rows from the query result
+
+	if len(results) == 0:   # if no rows found
 		printc("[yellow][-][/yellow] No results for the search")
 		return
-  
-  
-	if (decryptPassword and len(results)>1) or (not decryptPassword):
+
+	if (decryptPassword and len(results) > 1) or (not decryptPassword):
 		table = Table(title="Results")
 		table.add_column("Site Name")
 		table.add_column("URL",)
@@ -49,21 +49,21 @@ def retrieveEntries(mp, ds, search, decryptPassword = False):
 		table.add_column("Username")
 		table.add_column("Password")
     
-    for i in results:
-			table.add_row(i[0], i[1], i[2], i[3], "{hidden}")
+    for i in results:   # loop through each row of the query result
+			table.add_row(i[0], i[1], i[2], i[3], "{hidden}")   # add each row to the table with the password hidden
 		console = Console()
-		console.print(table)
-		return 
-  
-  if decryptPassword and len(results)==1:
+		console.print(table)   # print the table to the console
+		return
+
+	if decryptPassword and len(results) == 1:   # if only one row found and decrypt password is True
     
-    # Compute master key
-		mk = computeMasterKey(mp,ds)
+		# Compute master key
+		mk = computeMasterKey(mp, ds)   # derive the master key
 
 		# decrypt password
-		decrypted = utils.aesutil.decrypt(key=mk,source=results[0][4],keyType="bytes")
-    
-    pyperclip.copy(decrypted.decode())
-    printc("[green][+][/green] Password copied to clipboard")
-    
-db.close()
+		decrypted = utils.aesutil.decrypt(key=mk, source=results[0][4], keyType="bytes")   # decrypt the password using AES
+		pyperclip.copy(decrypted.decode())   # copy the decrypted password to the clipboard
+		printc("[green][+][/green] Password copied to clipboard
+		
+		# Close the database connection
+    db.close()
